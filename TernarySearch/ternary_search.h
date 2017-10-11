5b3d286dc26e312fb5a683bd2ca18318af79b161
phi <phi@phis-MacBook-Pro.local>
2017-10-10 18:46:21 -0700

    added ternary search

---
+++ TernarySearch/ternary_search.h
@@ -0,0 +1,32 @@
+#ifndef ternary_search_h
+#define ternary_search_h
+
+template <typename ArrayLike, typename T>
+int ternary_search(const ArrayLike& array, const T& value, int low, int high) {
+  unsigned int oneThird;
+  unsigned int twoThird;
+  while (low < high) {
+    oneThird = (low + ((high - low)/3));
+    twoThird = (low + 2*((high - low)/3));
+
+    if (low == high)
+      return -1;
+    else if (value == array[oneThird]) {
+      return oneThird;
+    }
+    else if (value < array[oneThird]) {
+      high = oneThird;
+    }
+    else if (value == array[twoThird]) {
+      return twoThird;
+    }
+    else if (value < array[twoThird]) {
+      low = oneThird + 1;
+      high = twoThird;
+    }
+    else { low = twoThird + 1; };
+  }
+  return -1;
+}
+
+#endif /* ternary_search_h */
