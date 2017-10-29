013e00f168de8075dde88f2ca0df9876c240d4b6
phi-line <phi.liney@gmail.com>
2017-10-29 15:15:37 -0700

    added mase MultiSet template

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by phi on 9/26/17.
+//  Copyright © 2017 phi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ MultiSet/MultiSet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MultiSet
+//
+//  Created by phi on 10/25/17.
+//  Copyright © 2017 phi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
