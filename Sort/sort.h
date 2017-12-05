c8e0f7384c38c71f683a7956c6e8de1194fcdbea
phi-line <phi.liney@gmail.com>
2017-12-04 20:24:29 -0800

    added check for is_sorted and is_reverse_sorted

--- Sort/sort.h
+++ Sort/sort.h
@@ -1,11 +1,38 @@
 #pragma once
 
 #include <algorithm>
+#include <cstdlib>
 #include <vector>
 
 using namespace std;
 
 namespace nonstd {
+    template <typename Comparable>
+    bool is_sorted(vector<Comparable>& a) {
+        int n = a.size();
+        if (n == 1 || n == 0)
+            return true;
+        
+        for (int i = 1; i < a.size(); i++)
+            if (a[i - 1] > a[i])
+                return false;
+        
+        return true;
+    }
+    
+    template <typename Comparable>
+    bool is_reverse_sorted(vector<Comparable>& a) {
+        int n = a.size();
+        if (n == 1 || n == 0)
+            return true;
+        
+        for (int i = 1; i < a.size(); i++)
+            if (a[i - 1] < a[i])
+                return false;
+        
+        return true;
+    }
+    
     template <typename Comparable>
     void percolate( vector<Comparable>& a, int i, int n ) {
         int child;
@@ -24,12 +51,22 @@ namespace nonstd {
     }
     
     template <typename Comparable>
-    void sort( vector<Comparable>& a ) {
+    void heapSort( vector<Comparable>& a ) {
         for( int i = a.size() / 2 - 1; i >= 0; --i )
             percolate<Comparable>( a, i, a.size() );
         for( int j = a.size() - 1; j > 0; --j ) {
             std::swap( a[0], a[j] );
             percolate<Comparable>( a, 0, j );
         }
+    }
+    
+    template <typename Comparable>
+    void sort( vector<Comparable>& a) {
+        if (is_sorted(a))
+            return;
+        else if (is_reverse_sorted(a))
+            std::reverse(a.begin(), a.end());
+        else
+            heapSort(a);
     }
 } // namespace nonstd
